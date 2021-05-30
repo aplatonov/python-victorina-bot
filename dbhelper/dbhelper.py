2722048d305e39ff83b8fb24853e35f9b1c17468
Anton Platonov <a.platonov@mediasoft.team>
2021-05-31 00:29:28 +0400

    рабочий вариант

--- dbhelper/dbhelper.py
+++ dbhelper/dbhelper.py
@@ -6,11 +6,14 @@ class DBHelper:
     questionFilename = 'dbhelper/question_data.sql'
 
     def __init__(self, dbname='db/victorina.sqlite'):
-        os.makedirs('../db', exist_ok=True)
+        os.makedirs('db', exist_ok=True)
         self.dbname = dbname
 
     def connect(self):
-        self.conn = sqlite3.connect(self.dbname)
+        try:
+            self.conn = sqlite3.connect(self.dbname)
+        except sqlite3.Error as error:
+            print("Ошибка при работе с SQLite", error)
 
     def setup(self):
         self.connect()
@@ -26,14 +29,58 @@ class DBHelper:
                 schema = file.read()
             self.conn.executescript(schema)
             self.conn.commit()
-
         self.disconnect()
 
-    def add_user(self, item_text):
-        stmt = "INSERT INTO user (user_id, user_name) VALUES (?)"
-        args = (item_text, )
+    def upsert_user(self, user_id, first_name, last_name, username):
+        self.connect()
+        stmt = "SELECT count(*) FROM user WHERE user_id=?"
+        args = (user_id, )
+        rowcount = self.conn.execute(stmt, args).fetchone()
+        if rowcount[0] == 0:
+            stmt = "INSERT INTO user (first_name, last_name, username, user_id) VALUES (?,?,?,?)"
+        else:
+            stmt = "UPDATE user SET first_name=?, last_name=?, username=? WHERE user_id=?"
+
+        args = (first_name, last_name, username, user_id, )
         self.conn.execute(stmt, args)
         self.conn.commit()
+        self.disconnect()
+
+    def get_questions_for_user(self, user_id, cnt):
+        self.connect()
+        stmt = "SELECT * FROM question ORDER BY RANDOM() LIMIT ?"
+        args = (cnt, )
+        questions = self.conn.execute(stmt, args).fetchall()
+        self.disconnect()
+        return questions
+
+    def upsert_user_points(self, user_id, question_id, points):
+        self.connect()
+        stmt = "SELECT count(*) FROM user_question WHERE user_id=? AND question_id=?"
+        args = (user_id, question_id, )
+        rowcount = self.conn.execute(stmt, args).fetchone()
+        if rowcount[0] == 0:
+            stmt = "INSERT INTO user_question (points, user_id, question_id) VALUES (?,?,?)"
+        else:
+            stmt = "UPDATE user_question SET points=? WHERE user_id=? AND question_id=?"
+
+        args = (points, user_id, question_id, )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+        self.disconnect()
+
+    def get_info(self, user_id):
+        self.connect()
+        stmt = "SELECT u.user_id, COUNT(uq.question_id), SUM(uq.points) FROM user u " + \
+               "LEFT JOIN user_question uq ON u.user_id = uq.user_id " + \
+               "WHERE u.user_id = ? " + \
+               "GROUP BY u.user_id"
+        args = (user_id, )
+        row = self.conn.execute(stmt, args).fetchone()
+        if row:
+            return dict(count=row[1], sum=row[2])
+        else:
+            return None
 
     def delete_user(self, user_id):
         self.connect()
@@ -42,10 +89,6 @@ class DBHelper:
         self.conn.execute(stmt, args)
         self.conn.commit()
         self.disconnect()
-
-    # def get_user(self):
-    #     stmt = "SELECT description FROM user"
-    #     return [x[0] for x in self.conn.execute(stmt)]
 
     def disconnect(self):
         self.conn.close()
--- main.py
+++ main.py
@@ -9,13 +9,31 @@ db = DBHelper()
 db.setup()
 db.disconnect()
 
+
 @bot.message_handler(commands=['start', 'help'])
 def start_command(message):
     bot.send_message(
         message.chat.id,
         'Вы можете поучаствовать в викторине.\n' +
-        'Для начала викторины наберите /go.\n' +
+        'Для получения вопроса наберите /go.\n' +
+        'ПОсмотреть свою статистику /info.\n' +
         'Для получения помощи наберите /help.'
+    )
+
+
+@bot.message_handler(commands=['info'])
+def start_command(message):
+    user_id = message.from_user.id
+    username = message.from_user.username
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    info = db.get_info(user_id)
+    if info != None:
+        bot.send_message(
+            message.chat.id,
+            'Пользователь ' + first_name + ' ' + last_name + ' (id: ' + str(user_id) + ', ' + username + '):\n' +
+            '\t-вопросов: ' + str(info.get("count")) + '\n' +
+            '\t-заработано баллов: ' + str(info.get("sum"))
     )
 
 
@@ -24,14 +42,43 @@ def go_command(message):
     global isRunning
     if not isRunning:
         isRunning = True
+        current_try = 1
+        user_id = message.from_user.id
+        username = message.from_user.username
+        first_name = message.from_user.first_name
+        last_name = message.from_user.last_name
         bot.send_message(
             message.chat.id,
-            'Так будет начинаться викторина.\n' +
-            'id:' + str(message.from_user.id) + '\n' +
-            'username: ' + message.from_user.username + '\n' +
-            'first_name + last_name ' + message.from_user.first_name + ' ' + message.from_user.last_name
+            'Вопрос для ' + first_name + ' ' + last_name + ':'
         )
+        db.upsert_user(user_id, first_name, last_name, username)
+        question = db.get_questions_for_user(user_id, int(1))[0]
+        sent = bot.send_message(message.chat.id, question[1])
+        bot.register_next_step_handler(sent, process_question, question, current_try)
         isRunning = False
+
+
+def process_question(message, question, current_try):
+    user_id = message.from_user.id
+    if current_try > config.MAX_TRY:
+        db.upsert_user_points(user_id, int(question[0]), 0)
+        bot.send_message(message.chat.id, 'Вы исчерпали все попытки.')
+        return
+
+    if message.text.lower() != str(question[2]).lower():
+        current_try += 1
+        sent = bot.send_message(
+            message.chat.id,
+            'Неверно, попробуйте еще. Осталось попыток: ' +
+            str(config.MAX_TRY - current_try + 1) +
+            '\n' +
+            question[1]
+        )
+        bot.register_next_step_handler(sent, process_question, question, current_try)
+        return
+    points = int(question[3]) // current_try
+    db.upsert_user_points(user_id, int(question[0]), points)
+    bot.send_message(message.chat.id, 'Верно, заработано баллов {}'.format(points))
 
 
 @bot.message_handler(content_types=['text'])
